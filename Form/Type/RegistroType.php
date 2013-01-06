062ebbcb9c5fca3e78b9803d3aa6589d9f24d468
juanda <juanda@transistor.(none)>
2013-01-06 18:57:01 +0100

    Adaptación formularios a 2.2.0

--- Form/Type/RegistroType.php
+++ Form/Type/RegistroType.php
@@ -3,11 +3,11 @@
 namespace Jazzyweb\AulasMentor\NotasFrontendBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
 
 class RegistroType extends AbstractType
 {
-    public function buildForm(FormBuilder $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('nombre', 'text')
                 ->add('apellidos', 'text')
